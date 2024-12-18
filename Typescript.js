0a52afc1e526f893b054f67c8747ec5eb05cb024
laibairfankhan <irfanlaiba978@gmail.com>
2024-12-18 18:50:57 +0500

    Your commit message here

---
+++ Typescript.js
@@ -0,0 +1,23 @@
+// get refernces to the form and display area
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+// handel form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // collect input value
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //generate the resume  dynamically
+    var resumeHTML = "\n    <h2><b>Editable Resume</h2>\n     <h3Personal Information</h3>\n      <p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n        <p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n          <p><b>Phone:</b><span contenteditable=\"true\">").concat(phone, "</span></p>\n\n          <h3>Education</h3>\n          <p contenteditable=\"true\">").concat(education, "</p>\n          \n          <h3>Education</h3>\n          <p contenteditable=\"true\">").concat(experience, "</p>\n          \n          <h3>Skills</h3>\n          <p contenteditable=\"true\">").concat(skills, "</p>\n          ");
+    //   display generate resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing.');
+    }
+});
